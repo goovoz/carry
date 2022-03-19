04a782b1343bd5f1824fc75c80040a5aa5e5cf2d
灰色的天空 <10392886+cxtwg2@user.noreply.gitee.com>
2022-03-19 20:02:48 +0800

    no commit message

--- jdYhqApiList.js
+++ jdYhqApiList.js
@@ -37,7 +37,7 @@ let apiList=[
 },             
    {
         "qName": "极速29-8",
-        "qTime": "9,12,17,20",//9,12,15,17,20
+        "qTime": "9,12,17",//9,12,15,17,20
         "lqSpace": 1,
         "endDate":"2022-03-31",
         "qApi": "https://api.m.jd.com/client.action?functionId=newBabelAwardCollection&client=wh5&body=%7B%22activityId%22%3A%223H885vA4sQj6ctYzzPVix4iiYN2P%22%2C%22scene%22%3A%221%22%2C%22args%22%3A%22key%3D6102ED3D9BC7C8DFF14D1A36FFA89602070B138DF76D200B6FA411359FBAD50B1FD83787E7C9BEF8843A4631ADF6C792_bingo%2CroleId%3D20E136F5EB78E9D323CEA65000B08718_bingo%2CstrengthenKey%3D19F512DCD8EE34ABE9C4FB4A92C2F42A65327F2097264F7157C058E713F98BE4_bingo%22%7D"
