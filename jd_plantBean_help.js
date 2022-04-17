373746b2db640092f3805686fe2f21453df6bfee
goovoz <67837389+goovoz@users.noreply.github.com>
2022-04-17 12:22:34 +0800

    Update jd_plantBean_help.js

--- jd_plantBean_help.js
+++ jd_plantBean_help.js
@@ -321,7 +321,7 @@ async function plantBeanIndex() {
 
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: `https://raw.githubusercontent.com/goovoz/updateTeam/master/bean`, timeout: 10000}, (err, resp, data) => {
+    $.get({url: `https://gh.mse.workers.dev/https://raw.githubusercontent.com/goovoz/updateTeam/master/bean`, timeout: 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err))
